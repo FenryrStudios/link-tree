75607fc8e90d7eb684a60a7075d306e35e6a2bfa
FenryrStudios <fenryrstudios@gmail.com>
2025-05-09 23:50:01 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function buttonPressed(site) {
+    if (site === "youtube") {
+        open("https://www.youtube.com/@FenryrStudios")
+    } else if (site === "github") {
+        open("https://github.com/FenryrStudios/FenryrStudios")
+    } else if (site === "linkedin") {
+        open("https://www.linkedin.com/in/christoffer-bamrud-310217333/")
+    }
+    
+}
